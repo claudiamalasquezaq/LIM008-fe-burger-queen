711e4fb714489129e849866326f2bb9791b2bf43
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-09 12:18:28 -0500

    Mostrando el menú en botones, al hacer clic, se muestra el item en consola

--- my-app/src/app/components/breakfast/breakfast.component.ts
+++ my-app/src/app/components/breakfast/breakfast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore/firestore.service';
 
 @Component({
   selector: 'app-breakfast',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreakfastComponent implements OnInit {
 
-  constructor() { }
+  public breakfastMenu = [];
+
+  constructor(
+    private firestoreService: FirestoreService
+  ) { }
+
+  getValueButton(value) {
+    console.log(value);
+    // return value;
+  }
 
   ngOnInit() {
+    this.firestoreService.getBreakfast()
+    .subscribe((breakfastSnapshot) => {
+      this.breakfastMenu = this.breakfastMenu.concat(breakfastSnapshot);
+      return this.breakfastMenu;
+    });
   }
 
 }
--- my-app/src/app/components/remainder/remainder.component.ts
+++ my-app/src/app/components/remainder/remainder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore/firestore.service';
 
 @Component({
   selector: 'app-remainder',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemainderComponent implements OnInit {
 
-  constructor() { }
+  public remainderMenu = [];
+  constructor(
+    private firestoreService: FirestoreService
+  ) { }
 
+  getValueButton(value) {
+    console.log(value);
+    // return value;
+  }
   ngOnInit() {
+    this.firestoreService.getRemainder()
+    .subscribe((remainderSnapshot) => {
+      this.remainderMenu = this.remainderMenu.concat(remainderSnapshot);
+      return this.remainderMenu;
+    });
   }
 
 }
