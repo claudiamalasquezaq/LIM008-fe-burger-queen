f5166738e7c80a1546a06fdadefae45589d72ea2
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-09 12:17:02 -0500

    Agregando configuración de proyecto en Firebase

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCdvnr1OwpGL0fYCaKHT81zih-jsFEq4Ew',
+    authDomain: 'burgenqueen-03-19.firebaseapp.com',
+    databaseURL: 'https://burgenqueen-03-19.firebaseio.com',
+    projectId: 'burgenqueen-03-19',
+    storageBucket: 'burgenqueen-03-19.appspot.com',
+    messagingSenderId: '253808216325'
+  }
 };
 
 /*
