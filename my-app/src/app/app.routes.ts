5cc59a3e1f9a58a97c3d57246c6700d492c03630
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-09 11:45:31 -0500

    Agregando nueva ruta para productos adicionales

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BreakfastComponent } from './components/breakfast/breakfast.component';
 import { RemainderComponent } from './components/remainder/remainder.component';
+import { AdditionalComponent } from './components/additional/additional.component';
 
 
 
 const routes: Routes = [
     { path: 'desayuno', component: BreakfastComponent },
     { path: 'otro-menu', component: RemainderComponent },
+    { path: 'adicionales', component: AdditionalComponent},
     { path: '**', component: BreakfastComponent }
 ];
 
