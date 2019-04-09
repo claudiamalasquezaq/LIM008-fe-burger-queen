0d680b3f9df992ef79bba2bed5017d833d9abd6d
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-09 12:18:01 -0500

    Agregando el servicio de firebase para traer la data del proyecto

---
+++ my-app/src/app/services/firestore/firestore.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(
+    private firedata: AngularFirestore
+  ) { }
+
+  // Obtiene el menú de Desayuno
+  getBreakfast() {
+    return this.firedata.collection('breakfast').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  getRemainder() {
+    return this.firedata.collection('remainder').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data};
+      }))
+    );
+  }
+
+  getAdditionalItems(){
+    return this.firedata.collection('additionalProducts').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data};
+      }))
+    );
+  }
+
+  getBurgerTaste(){
+    return this.firedata.collection('burgersTaste').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data};
+      }))
+    );
+  }
+
+}
