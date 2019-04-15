2a4bc98e11c9f5e6b8336c369165b3e8b161505c
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-14 23:11:06 -0500

    Agregando las rutas para hamburguesa simple y doble

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BreakfastComponent } from './components/breakfast/breakfast.component';
 import { RemainderComponent } from './components/remainder/remainder.component';
-import { AdditionalComponent } from './components/additional/additional.component';
+import { BurgerSimpleComponent } from './components/burger-simple/burger-simple.component';
+import { BurgerDoubleComponent } from './components/burger-double/burger-double.component';
 
 
 
 const routes: Routes = [
     { path: 'desayuno', component: BreakfastComponent },
     { path: 'otro-menu', component: RemainderComponent },
-    { path: 'adicionales', component: AdditionalComponent},
+    { path: 'otro-menu/hamb-simple', component: BurgerSimpleComponent},
+    { path: 'otro-menu/hamb-doble', component: BurgerDoubleComponent},
     { path: '**', component: BreakfastComponent }
 ];
 
