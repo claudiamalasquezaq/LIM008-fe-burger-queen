e6d9a6515ca5072fee2f780732e86f29e9fd86f1
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-14 23:18:36 -0500

    Agregando el método para que se muestre el producto seleccionado e intentando eliminar

--- my-app/src/app/components/order/order.component.ts
+++ my-app/src/app/components/order/order.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SendDataService } from '../../services/send-data.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
 
-  constructor() { }
+
+export class OrderComponent implements OnInit {
+  @Input() userName: string;
+
+  order: object[];
+
+  deleteProduct(arr, id) {
+    console.log(arr);
+    console.log(id);
+    const newArr = [];
+    arr.filter((elem) => {
+      if (elem.id !== id) {
+        newArr.push(elem);
+      }
+      return newArr;
+    });
+    console.log(newArr);
+  }
+
+  constructor(
+    private dataService: SendDataService
+  ) {
+    this.dataService.currentProduct.subscribe(prod => {
+      this.order = prod;
+      console.log(prod);
+    });
+   }
 
   ngOnInit() {
   }
+
 
 }
