9ae100e2236abb9a87be5d9ae156aa6b19854520
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-04-23 19:29:58 -0500

    Agregando la configuración de firebase para deployar

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCdvnr1OwpGL0fYCaKHT81zih-jsFEq4Ew',
+    authDomain: 'burgenqueen-03-19.firebaseapp.com',
+    databaseURL: 'https://burgenqueen-03-19.firebaseio.com',
+    projectId: 'burgenqueen-03-19',
+    storageBucket: 'burgenqueen-03-19.appspot.com',
+    messagingSenderId: '253808216325'
+  }
 };
